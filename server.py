13f51e2242b1efedb94e9d3d987c2183530b17a3
mgryszko <marcin.gryszko@lastminute.com>
2021-11-12 20:21:41 +0100

    Calculate Pakefte malus for last 100 rides of more 10 km in a REST API

--- server.py
+++ server.py
@@ -3,22 +3,32 @@ import os
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from stravalib.client import Client
+from strava_client import get_club_activities
+from score import rides_with_malus
 
 app = FastAPI()
-client = Client()
 
 CLIENT_ID = int(os.getenv("STRAVA_CLIENT_ID"))
 CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
-REDIRECT_URL = f"http://localhost:8000/authorized"
+REDIRECT_URL = os.getenv("STRAVA_OAUTH_REDIRECT_URL")
+
+CLUB_ID = int(os.getenv("CLUB_ID"))
+ACTIVITIES_LIMIT = int(os.getenv("ACTIVITIES_LIMIT"))
+CUTOFF_DISTANCE_M = 10_000
 
 
 @app.get("/")
-def read_root():
+def authorize():
+    client = Client()
     authorize_url = client.authorization_url(client_id=CLIENT_ID, redirect_uri=REDIRECT_URL)
     return RedirectResponse(authorize_url)
 
 
-@app.get("/authorized")
-def get_code(state=None, code=None, scope=None):
+@app.get("/malus")
+def get_pakefte_malus(code=None):
+    client = Client()
     token_response = client.exchange_code_for_token(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, code=code)
-    return token_response
+    client.access_token = token_response["access_token"]
+
+    activities = get_club_activities(client, CLUB_ID, ACTIVITIES_LIMIT)
+    return rides_with_malus(activities, CUTOFF_DISTANCE_M)
